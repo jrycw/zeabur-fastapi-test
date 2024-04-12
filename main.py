566012a6cf82b637386f4cb0329d3c8a89c1ec99
Jerry Wu <jerry@ycwu.space>
2024-04-12 16:30:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import datetime
+
 from fastapi import FastAPI
+from zoneinfo import ZoneInfo
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    tz = ZoneInfo("Asia/Taipei")
+    now = datetime.datetime.now(tz=tz)
+    return {"Datetime": f"{now:%Y:%m:%d-%H:%M:%S %Z%z}"}
